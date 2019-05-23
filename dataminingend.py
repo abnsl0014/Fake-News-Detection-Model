3627d888b174df2ab51aeed96b220fa530c60d87
Ayush Bansal <abnsl0014@gmail.com>
2019-05-24 01:46:36 +0530

    Add files via upload
    Using Natural Language Processing - with the help of NTLK toolkit and SVM model a fake news detection model is framed.

---
+++ dataminingend.py
@@ -0,0 +1,53 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('fakerealnews.csv')
+dataset = dataset[['news','label']]
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 2353):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['news'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer()
+vect =CountVectorizer(min_df=0.,max_df=1.0)
+X = cv.fit_transform(corpus)
+G = vect.fit_transform(corpus)
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+from sklearn.metrics import accuracy_score
+accuracy_score(y_test,y_pred)
